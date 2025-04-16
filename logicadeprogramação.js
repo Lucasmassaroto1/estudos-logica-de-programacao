3ecb2ab4b04e7b04588a06757c088b8cb23db0f8
Lucas Massaroto <lucasmassaroto17@gmail.com>
2025-04-16 08:42:33 -0300

    first commit

---
+++ logicadeprogramação.js
@@ -0,0 +1,18 @@
+/**
+ * =========Tipos de dados e Conceitos Básicos=========
+ * Variáveis
+ * - Let, Const, Var
+ * Tipos de dados
+ * - String, Number("Number(), parseInt(), parseFloat()"), Boolean(True ou False), Nulo, Undefined
+ * Operadores
+ * - Adição(+), Subtração(-), Multiplicação(*), Divisão(/), Resto da divisão(%)
+ * Operadores de Atribuição
+ * - =, +=, -=, *=, /=, %=, **=
+ * Operadores de Incremento e Decremento
+ * - ++, --
+ * Condição
+ * - If, Else if, Else
+ * Laços de repetição(Loop)
+ * Funções
+ * - Bloco reutilizáveis de Código
+ */
